a669df302c8e567ce5054a3bd37c75621c9edb4e
sontn <sontn@softdreams.vn>
2024-12-18 14:39:14 +0700

    update base

---
+++ database/seeders/BorrowSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Borrow;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class BorrowSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+        foreach (range(1, 100) as $index) {
+            Borrow::create([
+                'reader_id' => $faker->numberBetween(1, 10),
+                'book_id' => $faker->numberBetween(1, 10),
+                'borrow_date' => $faker->date(),
+                'return_date' => $faker->date()
+            ]);
+        }
+    }
+}
